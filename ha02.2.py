2ac31f74659a06402222f23fa0b433d24ef0b5e2
Mario Metshein <metshein@gmail.com>
2025-11-10 14:03:35 +0200

    h2.2

---
+++ ha02.2.py
@@ -0,0 +1,45 @@
+import turtle
+t = turtle.Turtle()
+aken = turtle.Screen()
+aken.title("Sinilill - Mario")
+aken.setup(500,500)
+t.speed(0)
+t.pensize(10)
+
+#vars
+t.penup()
+t.goto(0,-200)
+t.pendown()
+t.pencolor("green")
+t.lt(90)
+t.fd(200)
+#õis
+t.begin_fill()
+t.color("blue", "lightblue")
+t.rt(90)
+t.circle(60)
+t.end_fill()
+##emakas
+t.penup()
+t.goto(0,50)
+t.pendown()
+t.pencolor("yellow")
+t.pensize(20)
+t.circle(10)
+
+#leht
+t.penup()
+t.goto(-20,-100)
+t.pendown()
+t.pensize(10)
+t.begin_fill()
+t.color("green", "green")
+t.lt(90)
+t.fd(40)
+t.lt(135)
+t.fd(35)
+t.lt(100)
+t.fd(30)
+t.end_fill()
+
+turtle.done()
