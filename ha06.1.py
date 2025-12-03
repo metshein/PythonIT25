57764d619b7c29c340f713a7c449e8d4d31827ab
Mario Metshein <metshein@gmail.com>
2025-12-03 10:58:16 +0200

    sdfsadf

---
+++ ha06.1.py
@@ -0,0 +1,25 @@
+# 03.12 Metshein
+# Ülesanne 06
+import math
+import turtle
+
+turtle.speed(0)
+mootkava = 50
+
+nurk = math.radians(53)
+korgus = 4.4
+
+kaugus = korgus / math.tan(nurk)
+pikkus = round(math.sqrt(math.pow(korgus,2) + math.pow(kaugus,2)),2)
+
+print(pikkus)
+print(kaugus)
+print(nurk)
+
+turtle.fd(kaugus * mootkava)
+turtle.lt(90)
+turtle.fd(korgus * mootkava)
+turtle.lt(143)
+turtle.fd(pikkus * mootkava)
+
+turtle.done()
