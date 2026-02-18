6471029d9a3fcb3a2c4031361e9f69e5b7df3780
Mario Metshein <metshein@gmail.com>
2026-02-18 12:23:24 +0200

    asd

--- ha15.py
+++ ha15.py
@@ -55,6 +55,7 @@ def peegelda_porkumisel():
         # turtle.bye()
     x = ristkylik.xcor()
     y = ristkylik.ycor()
+    if (rist_y + pool_korgus >= pall_y and pall_y >= rist_y - pool_korgus) and (rist_x + pool_laius >= pall_x and pall_x >= rist_x - pool_laius)
     if (ring.ycor() <= y and ring.ycor()+5 >= y) and (ring.xcor() <= x and ring.xcor()+100 >= x):
         skoor+=1
         turtle.hideturtle()
